c13da07c09de46a318c1d49d3479b7578773b2f5
deaponn <bartek.sajo@interia.pl>
2023-01-13 13:28:13 +0100

    Fix no guard for undefined physicsSettings

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function createLayout(graph, physicsSettings) {
   var initialStep = 0;
 
   // below is a custom function that the user can create to control when to freeze the layout
-  var checkIfStable = typeof physicsSettings.checkIfStable === "function" ? physicsSettings.checkIfStable : (currentRatio) => currentRatio <= 0.01;
+  var checkIfStable = (!!physicsSettings && typeof physicsSettings.checkIfStable === "function") ? physicsSettings.checkIfStable : (currentRatio) => currentRatio <= 0.01;
 
   graph.on("changed", () => initialStep = 0); // reset the initial step after changing the graph
 
